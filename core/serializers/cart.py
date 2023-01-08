cc2b23074d023fce087bb14ed1db3e8e220711de
Akoh1 <akohsamuel018@gmail.com>
2023-01-08 18:28:16 +0100

    add to cart with same product

--- core/serializers/cart.py
+++ core/serializers/cart.py
@@ -3,6 +3,7 @@ from core.models.cart import Products, OrderItem, Cart
 from .auth import UserSerializer, BillAddressSerializer, BillAddressSerializerParser
 from datetime import datetime
 from typing import Dict, Any
+from django.shortcuts import get_object_or_404
 
 
 class StandardResultsSetPagination(pagination.PageNumberPagination):
@@ -78,22 +79,48 @@ class CartSerializer(serializers.ModelSerializer):
 
         return instance
 
-    # def update(self, instance, validated_data):
-    #     request = self.context['request']
-    #
-    #     # order_data = request.data.get('order')
-    #     # order_data = attempt_json_deserialize(order_data, expect_type=str)
-    #     # validated_data['order_id'] = order_data
-    #     #
-    #     # box_data = request.data.get('box')
-    #     # box_data = attempt_json_deserialize(box_data, expect_type=dict)
-    #     # box = Box.objects.create(**box_data)
-    #     # validated_data['box'] = box
-    #     #
-    #     # toppings_data = request.data.get('toppings')
-    #     # toppings_ids = attempt_json_deserialize(toppings_data, expect_type=list)
-    #     # validated_data['toppings'] = toppings_ids
-    #
-    #     instance = super().update(instance, validated_data)
-    #
-    #     return instance
+    def update(self, instance, validated_data):
+        request = self.context['request']
+        print(f"serilizer request: {request.data}")
+        product_slug = request.data.get("product_slug")
+        items_data = request.data.get("items")
+        print(f"update cart items: {instance.id}")
+        print(f"instance cart items: {instance.items}")
+        user = validated_data.get("user")
+        # product = get_object_or_404(Products, slug=product_slug)
+        # print(f"product : {product}")
+        # order_item =
+        # if OrderItem.objects.filter(product__slug=product_slug).exists():
+        # if instance.items.filter(product__slug=product_slug).exists():
+        #     print(f"product in cart already")
+        #
+        #     instance.items.num_of_prod += 1
+        #     # instance.items.num_of_prod += 1
+        #     # instance.items.save()
+        # else:
+        instance.items.add(items_data[0])
+        # validated_data["items"] = items_data
+        print(f"valid data: {validated_data}")
+        # instance.title = validated_data.get('title', instance.title)
+        # instance.code = validated_data.get('code', instance.code)
+        # instance.linenos = validated_data.get('linenos', instance.linenos)
+        # instance.language = validated_data.get('language', instance.language)
+        # instance.style = validated_data.get('style', instance.style)
+        instance.save()
+
+        # order_data = request.data.get('order')
+        # order_data = attempt_json_deserialize(order_data, expect_type=str)
+        # validated_data['order_id'] = order_data
+        #
+        # box_data = request.data.get('box')
+        # box_data = attempt_json_deserialize(box_data, expect_type=dict)
+        # box = Box.objects.create(**box_data)
+        # validated_data['box'] = box
+        #
+        # toppings_data = request.data.get('toppings')
+        # toppings_ids = attempt_json_deserialize(toppings_data, expect_type=list)
+        # validated_data['toppings'] = toppings_ids
+
+        # instance = super().update(instance, validated_data)
+
+        return instance
--- core/tests/order_tests.py
+++ core/tests/order_tests.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase, APIRequestFactory
 from core.models.auth import User
-from core.models.cart import Products
+from core.models.cart import Products, OrderItem, Cart
 from django.test import RequestFactory
 from core.helpers import RandomProductFactory, RandomUserFactory, CountryFactory, StateFactory, BillingAddressFactory
 
@@ -77,13 +77,63 @@ class OrderTests(APITestCase):
         self.client.post(address_url, data, format='json')
         cart_url = reverse('add-to-cart')
         cart_data = {
-            "product_slug":self.products[0].slug,
+            "product_slug": self.products[0].slug,
         }
-        print(f"cart data: {cart_data}")
+
         response = self.client.post(cart_url, cart_data, format='json')
-        print(f"res response: {response.data}")
+
         self.assertTrue(response.data['id'])
         self.assertEqual(len(response.data['items']), 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_add_to_cart_with_billing_address_cart_exists_same_product(self):
+
+        login_url = reverse('login')
+        data = {
+            'email': "test@gmail.com",
+            'password': "xxxx"
+        }
+        self.client.post(login_url, data, format='json')
+        data = {
+            'email': "test@gmail.com",
+            'password': "xxxx"
+        }
+        self.client.post(login_url, data, format='json')
+        address_url = reverse('billing-address')
+        data = {
+            'street': "37 Ogundola",
+            'state': self.state.id,
+            'country': self.country.id,
+            'city': "Bariga",
+            'zip_code': "100223",
+            'to_use': True
+        }
+        self.client.post(address_url, data, format='json')
+        cart_url = reverse('add-to-cart')
+        cart_data = {
+            "product_slug": self.products[0].slug,
+        }
+
+        response = self.client.post(cart_url, cart_data, format='json')
+        n_cart_data = {
+            "product_slug": self.products[0].slug,
+        }
+        # print(f"res response: {response.data}")
+        response = self.client.post(cart_url, n_cart_data, format='json')
+        print(f"sec response: {response.data}")
+        s_cart_data = {
+            "product_slug": self.products[0].slug,
+        }
+        # print(f"res response: {response.data}")
+        response = self.client.post(cart_url, s_cart_data, format='json')
+        print(f"tir response: {response.data}")
+        total_amount = self.products[0].price * 3
+        order_items = OrderItem.objects.get(id=response.data['items'][0].get('id'))
+        # cart = Cart.objects.get(id=response.data["id"])
+        # print(f"test order items: {cart.items}")
+        # self.assertTrue(response.data['id'])
+        self.assertEqual(order_items.num_of_prod, 3)
+        self.assertEqual(response.data['total_amount'], total_amount)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def tearDown(self):
